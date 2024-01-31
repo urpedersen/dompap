f53c759a32be32198a40f059f9fbee0bad037c3a
Ulf R. Pedersen <ulf@urp.dk>
2024-01-31 20:18:36 +0100

    Update __init__.py

--- dompap/__init__.py
+++ dompap/__init__.py
@@ -1,6 +1,16 @@
-from dompap.Simulation import Simulation
-from dompap.to_lammps_dump import to_lammps_dump
-from dompap.progress_bar import progress_bar
-from dompap.autotuner import autotune
-from dompap.main import main
-from dompap.run_test_simulation import run_test_simulation
+"""
+dompap
+======
+
+Simulations of point-like particles in any dimension with any pair potential.
+The package uses NumPy and Numba for efficient calculations and SymPy to implement any pair potentials.
+The user is not expected to be familiar with these packages but only basic Python syntax.
+
+Repository: <https://github.com/urpedersen/dompap>
+"""
+from .Simulation import Simulation
+from .to_lammps_dump import to_lammps_dump
+from .progress_bar import progress_bar
+from .autotuner import autotune
+from .main import main
+from .run_test_simulation import run_test_simulation
