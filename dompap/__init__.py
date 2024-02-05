61b12576d902ba423f7e003c790f8310da620991
Ulf R. Pedersen <ulf@urp.dk>
2024-02-05 15:57:05 +0100

    Start on version 0.0.6

--- dompap/__init__.py
+++ dompap/__init__.py
@@ -9,7 +9,7 @@ The user is not expected to be familiar with these packages but only basic Pytho
 Repository: <https://github.com/urpedersen/dompap>
 """
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = 'Ulf R. Pedersen'
 
 from .Simulation import Simulation
