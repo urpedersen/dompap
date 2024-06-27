BUGFIX: Laplacian for configurational temperature was computed a factor 2 to large, making T_conf a factor 2 to small.

dompap/potential.py
@@ -285,7 +285,7 @@ def _get_forces_vectorized(positions, box_vectors, pair_force, sigma_func, epsil
     return forces
 
 
-@numba.njit
+@numba.njit(parallel=True)
 def _get_laplacian(positions: np.ndarray,
                    box_vectors: np.ndarray,
                    pair_force: callable,
@@ -296,20 +296,28 @@ def _get_laplacian(positions: np.ndarray,
     """ Return the Laplacian of for each particle """
     number_of_particles = positions.shape[0]
     laplacians = np.zeros(number_of_particles, dtype=np.float64)
-    dim_of_space = positions.shape[1]
+    dimension_of_space = positions.shape[1]
     for n in numba.prange(number_of_particles):
         positions_n = positions[n]
+        this_lap = 0.0
         for m in neighbor_list[n]:
             if m == -1:
                 break
             position_m = positions[m]
             displacement = get_displacement_vector(positions_n, position_m, box_vectors)
-            distance = np.sum(displacement ** 2) ** 0.5
+            distance = 0.0
+            for d in range(dimension_of_space):
+                if displacement[d] > box_vectors[d] / 2:
+                    displacement[d] -= box_vectors[d]
+                elif displacement[d] < -box_vectors[d] / 2:
+                    displacement[d] += box_vectors[d]
+                distance += displacement[d] ** 2
+            distance = distance ** 0.5
             sigma = sigma_func(n, m)
             epsilon = epsilon_func(n, m)
             scalar_force = epsilon * pair_force(distance / sigma)
             dv_dr = -scalar_force
             dv2_dr2 = epsilon * pair_curvature(distance / sigma)
-            this_lap = (dv2_dr2 + (dim_of_space-1)*dv_dr/distance)*2.0
-            laplacians[n] += this_lap
+            this_lap += dv2_dr2 + (dimension_of_space-1)*dv_dr/distance
+        laplacians[n] = this_lap
     return laplacians

examples/configurational_temperature.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from dompap import Simulation
 from dompap.tools import autotune
 
-
 # Setup Lennard-Jones simulation
 sim = Simulation()
 fcc_unit_cell = np.array([
@@ -19,14 +18,14 @@ specific_volume = 1.0277
 density = 1.0 / specific_volume
 sim.set_density(density=density)
 sim.set_masses(masses=1.0)
-sim.set_random_velocities(temperature=temperature*2)
+sim.set_random_velocities(temperature=temperature * 2)
 sim.set_pair_potential(pair_potential_str='4*(r**-12-r**-6)', r_cut=2.5)
 sim.set_pair_potential_parameters(sigma=1.0, epsilon=1.0)
 sim.set_neighbor_list(skin=0.6, max_number_of_neighbors=128)
 sim.set_integrator(time_step=0.004, target_temperature=temperature, temperature_damping_time=2.0)
 
 # Equilibrate
-sim.run(200)
+sim.run(1000)
 
 # Run simulation
 times = []
@@ -37,7 +36,7 @@ pressures = []
 N = sim.get_number_of_particles()
 steps = 1000
 for step in range(steps):
-    if step % (steps//100) == 0:
+    if step % (steps // 100) == 0:
         times.append(sim.get_time())
         pressure = sim.get_pressure()
         print(f'Pressure: {pressure}')
@@ -45,7 +44,7 @@ for step in range(steps):
         print(f'  Step {step}:')
         E_pot = sim.get_potential_energy()
         E_pots.append(E_pot)
-        print(f'Energy per particle         : {E_pot/N}')
+        print(f'Energy per particle         : {E_pot / N}')
         T_conf = sim.get_configurational_temperature()
         T_confs.append(T_conf)
         print(f'Configurational temperature : {T_conf}')
@@ -53,14 +52,10 @@ for step in range(steps):
         T_kins.append(T_kin)
         print(f'Kinetic temperature         : {sim.get_temperature()}')
 
-        #F = sim.get_forces()
-        #print(f'Average of force square: {np.sum(F**2)/N}')
-        #lap = sim.get_laplacian()
-        #print(f'Average of particle laplacians: {np.sum(lap)/N}')
     sim.step()
 
 # Print summary statistics
-print(f'Average potential energy per particle: U = {np.mean(E_pots)/N}')
+print(f'Average potential energy per particle: U = {np.mean(E_pots) / N}')
 expected_potential_energy = -4.953 - 0.8 * 3 / 2
 print(f'{expected_potential_energy = }')
 print(f'Pressure = {np.mean(pressures)}')
@@ -68,28 +63,19 @@ expected_pressure = 2.185
 print(f'{expected_pressure = }')
 print(f'Average configurational temperature: T_c = {np.mean(T_confs)}')
 print(f'Average kinetic temperature: T_k = {np.mean(T_kins)}')
-print(f'T_k/T_c ratio: {np.mean(T_kins)/np.mean(T_confs)}')
+print(f'T_k/T_c ratio: {np.mean(T_kins) / np.mean(T_confs)}')
 
 # Print standard deviations
-print(f'Standard deviation of potential energy per particle: U = {np.std(E_pots)/N}')
+print(f'Standard deviation of potential energy per particle: U = {np.std(E_pots) / N}')
 print(f'Standard deviation of configurational temperature: T_c = {np.std(T_confs)}')
 print(f'Standard deviation of kinetic temperature: T_k = {np.std(T_kins)}')
-
 
 # Plot configurational temperature and kinetic temperature
 plt.figure()
 plt.plot(times, T_confs, label='Configurational temperature')
-plt.plot(times, np.array(T_confs)*2.0, label='Configurational temperature x2')
 plt.plot(times, T_kins, label='Kinetic temperature')
 plt.xlabel('Time')
 plt.ylabel('Temperature')
 plt.legend()
 plt.xlabel('Time')
-plt.show()
-
-# Plot positions
-plt.figure()
-plt.plot(sim.positions[:, 0], sim.positions[:, 1], 'o')
-plt.xlabel('x')
-plt.ylabel('y')
 plt.show()